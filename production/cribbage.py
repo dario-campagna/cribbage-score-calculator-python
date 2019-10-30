0923e2fa3f110534e9904fd5ab6497990ffe524f
Dario Campagna <campagna@esteco.com>
2019-10-30 17:13:24 +0100

    We now parse the suite of a card
    Signed-off-by: Dario Campagna <campagna@esteco.com>

--- production/cribbage.py
+++ production/cribbage.py
@@ -1,8 +1,19 @@
+from enum import Enum
+
+
 def parse_card(card_as_text):
-    return Card(card_as_text[0])
+    return Card(card_as_text[0], Suite(card_as_text[1]))
+
+
+class Suite(Enum):
+    HEARTS = '♥'
+    CLUBS = '♣'
+    DIAMONDS = '♦'
+    SPADES = '♠'
 
 
 class Card(object):
-    def __init__(self, rank):
+    def __init__(self, rank, suite):
         super()
         self.rank = rank
+        self.suite = suite
--- test/test_parse.py
+++ test/test_parse.py
@@ -1,12 +1,29 @@
 import unittest
 
-from production.cribbage import parse_card
+from production.cribbage import parse_card, Suite
 
-class TestParse(unittest.TestCase):
+
+class TestParseCard(unittest.TestCase):
     def test_parse_rank_5(self):
-        card = parse_card('5♥️')
+        card = parse_card('5♥')
         self.assertEqual('5', card.rank)
-    
+
     def test_parse_rank_A(self):
-        card = parse_card('A♥️')
+        card = parse_card('A♥')
         self.assertEqual('A', card.rank)
+
+    def test_parse_suite_hearts(self):
+        card = parse_card('0♥')
+        self.assertEqual(Suite.HEARTS, card.suite)
+
+    def test_parse_suite_clubs(self):
+        card = parse_card('0♣︎')
+        self.assertEqual(Suite.CLUBS, card.suite)
+    
+    def test_parse_suite_diamonds(self):
+        card = parse_card('0♦')
+        self.assertEqual(Suite.DIAMONDS, card.suite)
+
+    def test_parse_suite_spades(self):
+        card = parse_card('0♠')
+        self.assertEqual(Suite.SPADES, card.suite)
