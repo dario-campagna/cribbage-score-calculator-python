da9081e3e732604d4708d952ee73129e1d8d53c7
Dario Campagna <campagna@esteco.com>
2019-10-30 16:59:00 +0100

    We now parse the rank of a card

--- production/cribbage.py
+++ production/cribbage.py
@@ -1,5 +1,6 @@
 def parse_card(card_as_text):
-    return Card(5)
+    return Card(card_as_text[0])
+
 
 class Card(object):
     def __init__(self, rank):
--- test/test_parse.py
+++ test/test_parse.py
@@ -5,4 +5,8 @@ from production.cribbage import parse_card
 class TestParse(unittest.TestCase):
     def test_parse_rank_5(self):
         card = parse_card('5♥️')
-        self.assertEqual(5, card.rank)
+        self.assertEqual('5', card.rank)
+    
+    def test_parse_rank_A(self):
+        card = parse_card('A♥️')
+        self.assertEqual('A', card.rank)
