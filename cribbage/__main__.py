220c1087689f1e1e58a2d5dc3998eefb267e1197
Dario Campagna <campagna@esteco.com>
2019-11-04 10:25:00 +0100

    Refactoring
    Signed-off-by: Dario Campagna <campagna@esteco.com>

--- cribbage/__main__.py
+++ cribbage/__main__.py
@@ -3,10 +3,10 @@ from cribbage.score import score
 from cribbage.parse import parse_hand
 
 
-def main(hand_as_string):
+def compute_score(hand_as_string):
     points = score(parse_hand(hand_as_string))
     print('The score is ' + str(points))
 
 
 if __name__ == "__main__":
-    main(sys.argv[1])
+    compute_score(sys.argv[1])
--- test/test_main.py
+++ test/test_main.py
@@ -6,5 +6,5 @@ from cribbage import __main__
 class TestMain(unittest.TestCase):
     @patch('builtins.print')
     def test_compute_score(self, mock_print):
-        __main__.main('5♥5♦5♣J♠5♠')
+        __main__.compute_score('5♥5♦5♣J♠5♠')
         mock_print.assert_called_with('The score is 29')
